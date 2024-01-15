588242bd8282db27c8d28627b6c5ba3f2946babb
maish <trondoor@gmail.com>
2024-01-15 15:22:45 +0530

    1st one

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -29,7 +29,7 @@ router.post(
         return res.status(400).json({ success,error: "Email already exixits" });
       }
       const salt = await bcrypt.genSalt(10);
-      secPass = await bcrypt.hash(req.body.password, salt);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
@@ -102,7 +102,7 @@ router.post(
     fetchUser,
     async (req, res) => {
 try{
-    userId = req.user.id;
+    const userId = req.user.id;
     const user =  await User.findById(userId).select("-password");
     res.send(user)
 }catch(error){
