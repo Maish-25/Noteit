c2dd312d9e9443d38be2ab8aa232b45477ee9206
maish <trondoor@gmail.com>
2024-01-16 21:32:47 +0530

    3nd

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ connectToMongo();
 const express = require('express')
 var cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3001;
 
 app.use(cors())
 app.use(express.json())
